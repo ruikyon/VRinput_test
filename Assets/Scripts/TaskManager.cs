6ac8fc72f283c06cdcc8246eddfc5335935d512c
ruikyon <meranzunx@gmail.com>
2020-01-06 12:33:18 +0900

    タスク開始を『S」キーに変更。タスク終了時にresult.txtに現在時刻とともに結果を出力

--- Assets/Scripts/TaskManager.cs
+++ Assets/Scripts/TaskManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
 
 public class TaskManager : MonoBehaviour
 {
@@ -9,19 +11,28 @@ public class TaskManager : MonoBehaviour
     private int taskCount;
     private float start, end;
     public static TaskManager Instance { get; private set; }
+    private bool flag;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Debug.Log(System.Environment.CurrentDirectory);
+        //File.AppendAllText("sample.txt", "test");
+
         Instance = this;
         taskCount = 0;
-        Scheduler.AddEvent(StartTask, 5);
+        //Scheduler.AddEvent(StartTask, 5);
+        flag = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!flag && Input.GetKeyDown(KeyCode.S))
+        {
+            StartTask();
+            flag = true;
+        }
     }
 
     private void StartTask()
@@ -36,7 +47,7 @@ public class TaskManager : MonoBehaviour
         end = Time.time;
         Debug.Log("result time: " + (end - start));
         Debug.Log("-task end-");
-
+        File.AppendAllText("result.txt", DateTime.Now.ToString() + ": " + (end - start) + Environment.NewLine);
     }
 
     public void NextTask()
